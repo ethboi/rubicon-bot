08e7d42b199c312ebd9ec01e1256fb49965719c5
ethboi <ethboi3@gmail.com>
2023-04-04 17:37:37 +0200

    update logger

--- src/app.ts
+++ src/app.ts
@@ -6,20 +6,30 @@ import { LOG_CHANNEL, LOG_TOKEN } from './secrets'
 async function Initialize(): Promise<void> {
   try {
     RegisterShutdownEvents()
-    await Notifier(false)
+    await Notifier('Initialize', false)
     await Run()
   } catch (error) {
     console.error(error)
+    await Notifier('Initialize')
+    throw error // do this to restart the app!
   }
 }
 
-async function Notifier(isDown = true) {
-  await PostTelegram(`🟥 Rubicon Bot ${isDown ? 'Down' : 'Up'}\n`, new Telegraf(LOG_TOKEN), LOG_CHANNEL)
+async function Notifier(message: string, isDown = true) {
+  await PostTelegram(`🟥 Rubicon Bot ${isDown ? 'Down' : 'Up'} (${message}) \n`, new Telegraf(LOG_TOKEN), LOG_CHANNEL)
 }
 
 function RegisterShutdownEvents(): void {
+  process.on('uncaughtException', async (error) => {
+    console.error('Uncaught exception! Shutting down.')
+    await Notifier('Uncaught Exception')
+    await PostTelegram(`Error:\n\n ${error.message}`, new Telegraf(LOG_TOKEN), LOG_CHANNEL)
+    console.error(error)
+  })
+
   process.on('beforeExit', async (code) => {
-    await Notifier().then(process.exit(code))
+    console.log('before exit.')
+    await Notifier('Before Exit').then(process.exit(code))
   })
 }
 
